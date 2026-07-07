3c52060ee589eb62d42bee2e7d0baea66ac0cda4
nikishdaniel1-del <nikishdaniel1@gmail.com>
2026-07-07 18:00:04 +0530

    updated

---
+++ python class/Format.py
@@ -0,0 +1,9 @@
+string1 = 'Name : {name} Age : {age}'
+print(string1.format(name="Daniel",age=22))
+print('Name : {1} Age : {0}'.format('22','Daniel'))
+
+print(f'{10:b}')
+name = 'Daniel'
+age = 22
+print(f'Name : {name} Age : {age}')
+
