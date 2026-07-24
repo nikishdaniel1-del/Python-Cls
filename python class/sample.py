7c4a2815584de7118078ce98368d5e05517939ab
nikishdaniel1-del <nikishdaniel1@gmail.com>
2026-07-24 16:28:10 +0530

    updated

--- python class/sample.py
+++
@@ -1,2 +0,0 @@
-def add(number1,number2):
-    return number1+number2
--- python class/test_sample.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from sample import add
-
-@pytest.mark.parametrize("operation,a,b,expected",[(add,2,6,8),(add,-2,-3,-5),(add,5,0,5),(add,-1,3,2)])
-
-def test_operations(operation,a,b,expected):
-    assert operation(a,b)==expected
