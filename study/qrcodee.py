cd8150d328ad998968fd8710ac6db5cc866ac25e
nikishdaniel1-del <nikishdaniel1@gmail.com>
2026-07-29 16:58:25 +0530

    updated

--- study/qrcodee.py
+++ study/qrcodee.py
@@ -3,4 +3,4 @@ import qrcode
 qr = qrcode.QRCode(version=5,box_size=5,border=5)
 qr.add_data('hi hello')
 qr.make_image(fill_color='blue',back_color='white').save('customQR.png')
-qrcode.make('mailto:nikishdaniel1@gmail.com?subject=Hello Nikish Daniel').save('qrImage.png')
+qrcode.make('mailto:user@gmail.com?subject=Hello user').save('qrImage.png')
