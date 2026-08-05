4ed0b887bd668895d20bea774236f72bde6e14f6
nikishdaniel1-del <nikishdaniel1@gmail.com>
2026-08-05 18:01:13 +0530

    updated

--- python class/Excel.py
+++ python class/Excel.py
@@ -12,28 +12,28 @@ if not os.path.exists(path):
     workBook[workBook.sheetnames[0]].title = 'Data1'
     workBook.save(path)
 workBook = load_workbook(path)
-workSheet = workBook.active
-if 'Employee_Table' not in workSheet.tables.keys():
-    workSheet.append(['ID','Name','Age','Languages Known','Gender','Comments'])
-    genderValidater = DataValidation(type='list',formula1='"Male,Female"',allow_blank=True)
-    workSheet.add_data_validation(genderValidater)
-    genderValidater.add('E2:E1048576')
-    table = Table(displayName='Employee_Table',ref='A1:F1')
-    table.tableStyleInfo = TableStyleInfo(name='TableStyleMedium15',showFirstColumn=False,showLastColumn=False,showColumnStripes=True,showRowStripes=True)
-    workSheet.add_table(table)
-else:table = workSheet.tables['Employee_Table']
 # workBook.create_sheet('New Next Sheet')
-workSheet.title = 'First sheet'
-# print(workBook.sheetnames)
+workSheet = workBook['New Next Sheet']
+workBook.active = workBook.index(workSheet)
+workSheet = workBook.active
+if 'Sales_Table' not in workSheet.tables.keys():
+    workSheet.append(['Sales ID','Company Email','Quantity','Price'])
+    # genderValidater = DataValidation(type='list',formula1='"Male,Female"',allow_blank=True)
+    # workSheet.add_data_validation(genderValidater)
+    # genderValidater.add('E2:E1048576')
+    table = Table(displayName='Sales_Table',ref='A1:D1')
+    table.tableStyleInfo = TableStyleInfo(name='TableStyleMedium10',showFirstColumn=False,showLastColumn=False,showColumnStripes=True,showRowStripes=True)
+    workSheet.add_table(table)
+else:table = workSheet.tables['Sales_Table']
+# workBook.create_sheet('New Next Sheet')
+# workSheet.title = 'First sheet'
 # workSheet = workBook['New Next Sheet']
-# print(workBook.active)
 # workBook.active = workBook.index(workSheet)
-# print(workBook.active)
 generator = Faker()
-for i in range(15):workSheet.append([generator.random_int(1,1000),generator.name(),generator.random_int(20,70),generator.language_name(),'',''])
-for i in workSheet.iter_rows():print(i[0].value)
+for i in range(15):workSheet.append([generator.iban(),generator.company_email(),generator.random_int(1,20),generator.pricetag()])
+# for i in workSheet.iter_rows():print(i[0].value)
 table.ref = f'A1:{get_column_letter(workSheet.max_column)}{workSheet.max_row}'
 # workSheet.delete_rows(1,4)
-workSheet['F2'].comment = Comment(text='This is a Comment',author='Nikish Daniel')
+# workSheet['F2'].comment = Comment(text='This is a Comment',author='Nikish Daniel')
 workBook.save(path)
 workBook.close()
