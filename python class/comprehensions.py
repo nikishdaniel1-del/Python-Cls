45be0a4f7e6cd65f010fd65c61b0389707118799
nikishdaniel1-del <nikishdaniel1@gmail.com>
2026-08-24 18:00:52 +0530

    updated

---
+++ python class/comprehensions.py
@@ -0,0 +1,23 @@
+# list
+'''print([x for x in range(1,5)])'''
+
+# dictionary
+'''print({x:chr(x+97) for x in range(26)})'''
+
+# set
+'''print({x for x in 'abbacd'})'''
+
+# generators
+'''stream = (x for x in range(5))
+for i in stream:
+    print(i)'''
+    
+print(['odd' if x%2 else 'even' for x in range(1,5)])
+
+print([x for x in range(1,5) if x%2])
+
+def sample(x):
+    if x==1:return 'One'
+    elif x==2:return 'Two'
+    else:return 'Three'
+print([sample(x) for x in range(1,4)])
