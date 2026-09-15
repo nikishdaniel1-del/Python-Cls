e48f7d4dd93525990bf6b9cd479e4f011bd67fb9
nikishdaniel1-del <nikishdaniel1@gmail.com>
2026-09-15 14:27:46 +0530

    updated

--- python class/webgui.py
+++ python class/webgui.py
@@ -38,10 +38,10 @@ def main():
     pdfWidgets = ['Text','Table','Link','Image','Line Break']
     with ui.row().classes('w-full gap-1'):
         with ui.card().classes('w-full').style('background-color: rgba(255,255,255,0.9); backdrop-filter: blur(0.5px); border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.5);'):
-            with ui.grid(columns='10% 5% 5% 30% 10% 15% 15%').classes('w-full gap-2'):
+            with ui.grid(columns='10% 10% 8% 30% 10% 15%').classes('w-full gap-2'):
                 ui.button('Back',color='#00fff2',on_click=lambda:ui.navigate.to('/'),icon='arrow_back')
                 ui.space().classes('w-full')
-                ui.label('Widgets').classes('font-bold').props('dense')
+                ui.label('Widgets').classes('font-bold').props('dense').style('font-family:"Times New Roman";font-size:26px;font-weight:bold;')
                 ui.select(label='Select Widget',options=pdfWidgets,clearable=True,on_change=lambda x:add(x.value)).props('dense')
                 ui.space().classes('w-full')
                 ui.button('Generate PDF',on_click=generatePDF).classes('w-full')
@@ -51,7 +51,24 @@ def main():
                 text = ui.textarea(placeholder='Enter your Text here')
                 ui.button('Fetch',on_click=lambda:print(text.value))
             with ui.card().classes('w-full h-screen overflow-auto').style('background-color: rgba(1,1,1,0.6); backdrop-filter: blur(0.5px); border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.5);'):
-                pdfViewer = ui.html('',sanitize=False).classes('w-full h-full')               
+                pdfViewer = ui.html('',sanitize=False).classes('w-full h-full')
+
+@ui.page('/Register')
+def register():
+    def pushData():
+        currentUserName = userName.value
+        currentPassword = password.value
+        if currentUserName=='' or currentPassword=='':ui.notify('Please fill in all fields',type='negative');return
+        connection = poolConnections.get_connection()
+        cursor = connection.cursor()
+        cursor.execute(f"INSERT INTO users (username,password) VALUES ('{currentUserName}','{currentPassword}')")
+        connection.commit()
+        cursor.close()
+        connection.close()
+    with ui.card().classes('absolute-center w-[50%] items-center').style('background-color: rgba(1, 1, 1, 0.7); backdrop-filter: blur(1px); border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.5);'):
+        userName = ui.input(label='UserName',placeholder='Enter your UserName').classes('w-full white-input').props('clearable')
+        password = ui.input(label='Password',placeholder='Enter your Password',password=True,password_toggle_button=True).classes('w-full white-input').props('clearable')
+        ui.button('Register',icon='person_add',on_click=pushData).classes('w-1/4')
 
 @ui.page('/')
 def home():
@@ -67,7 +84,7 @@ def home():
         ui.input(label='UserName',placeholder='Enter your UserName',value='Daniel').classes('w-full white-input').props('clearable')
         ui.input(label='Password',placeholder='Enter your Password',password=True,password_toggle_button=True).classes('w-full white-input').props('clearable')
         with ui.row().classes('w-full gap-2 justify-center'):
-            ui.button('Register',icon='person_add').classes('w-1/4')
+            ui.button('Register',icon='person_add',on_click=lambda:ui.navigate.to('/Register')).classes('w-1/4')
             ui.button('Login',icon='login',on_click=lambda:ui.navigate.to('/Main'),color="white").classes('w-1/4')
         ui.link('Forgot Password?')
 
